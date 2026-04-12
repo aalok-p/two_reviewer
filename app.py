7b4b4f05341e4f0f3a5906f1cb6c048f47780a10
11PRIMUS <alokpal2803@gmail.com>
2026-04-12 18:59:23 +0530

    feat: enables share mode on hf

--- app.py
+++ app.py
@@ -87,4 +87,5 @@ if __name__ == "__main__":
         show_error=True,
         server_name="0.0.0.0",
         server_port=int(os.getenv("PORT", "7860")),
+        share=bool(os.getenv("SPACE_ID")),
     )
